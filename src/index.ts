8728a698a428ccf3f845db1c3de8e7965def3fb4
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-01-23 13:39:35 +0100

    test commit

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000
 const startApp = async ()=> {
     await runDb()
     app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
+        console.log(`We started app and listening on port ${port}`)
     })
 }
 
