64b84fddd77cff822e3073f72adb36b41b48feec
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-03-31 17:39:19 +0200

    hw#13 nest v1.5

--- src/features/dropAll/api/testing-all-data.controller.ts
+++ src/features/dropAll/api/testing-all-data.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Delete } from "@nestjs/common";
-import { UsersRepository } from "../../users/infrastructure/users.repository";
-import { BlogsRepository } from "../../blogs/infrastructure/blogs.repository";
-import { PostsRepository } from "../../posts/infrastructure/posts.repository";
+import { Controller, Delete, HttpCode } from '@nestjs/common';
+import { UsersRepository } from '../../users/infrastructure/users.repository';
+import { BlogsRepository } from '../../blogs/infrastructure/blogs.repository';
+import { PostsRepository } from '../../posts/infrastructure/posts.repository';
 
-@Controller("/testing/all-data")
+@Controller('/testing/all-data')
 export class TestingAllDataController {
-  constructor(private userRepository: UsersRepository, private blogRepository: BlogsRepository, private postRepository: PostsRepository) {
-  }
+  constructor(
+    private userRepository: UsersRepository,
+    private blogRepository: BlogsRepository,
+    private postRepository: PostsRepository,
+  ) {}
 
   @Delete()
+  @HttpCode(204)
   async deleteAllBD() {
     await this.blogRepository.deleteAll();
     await this.userRepository.deleteAll();
